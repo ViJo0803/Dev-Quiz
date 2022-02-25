2204b7477dc12812e4300e7f2377949cdd7d34da
ViJo0803 <vijo967@gmail.com>
2022-02-25 13:17:54 -0500

    iniciando el proyecto

---
+++ Dev/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import {StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+
+export default class Touchables extends Component {
+  _onPressButton() {
+    alert('You tapped the button!')
+  }
+
+  _onLongPressButton() {
+    alert('You long-pressed the button!')
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableHighlight</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 60,
+    alignItems: 'center'
+  },
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+  buttonText: {
+    textAlign: 'center',
+    padding: 20,
+    color: 'white'
+  }
+});
